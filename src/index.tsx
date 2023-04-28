5419ae6128bd6efaf3c628c014b6bf0ae6cd0caf
stakbucks <dhassidu11@gmail.com>
2023-04-28 17:05:17 +0900

    feat: router생성 후 초기 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,4 @@
+function NotFound() {
+  return <h1>404 NOT FOUND</h1>;
+}
+export default NotFound;
