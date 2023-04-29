fb59f4d05178e9d90129c6a5e5e9d72dad50cf2b
stakbucks <dhassidu11@gmail.com>
2023-04-29 15:15:11 +0900

    feat: 회원가입 두 페이지로 폼 나누기

--- src/components/Modal/SignupForm/SignupForm.tsx
+++ src/components/Modal/SignupForm/SignupForm.tsx
@@ -1,4 +1,5 @@
-import { ISignupForm } from "../../../interfaces/forms";
+import { useState } from "react";
+import { ISignupFormFirst, ISignupFormSecond } from "../../../interfaces/forms";
 import * as S from "./style";
 import { useForm } from "react-hook-form";
 function SignupForm({
@@ -6,106 +7,116 @@ function SignupForm({
 }: {
   setIsSigningup: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
+  const [isFirstStage, setIsFirstStage] = useState<boolean>(true);
   const handleGoBackClick = () => setIsSigningup(false);
   const {
-    register,
-    handleSubmit,
+    register: firstRegister,
+    handleSubmit: firstHandleSubmit,
     formState: { errors },
     trigger,
     setError,
-  } = useForm<ISignupForm>();
-
-  const onValid = (data: ISignupForm) => {
+  } = useForm<ISignupFormFirst>();
+  const {
+    register: secondRegister,
+    handleSubmit: secondHandleSubmit,
+    trigger: secondTrigger,
+    formState: { errors: secondErrors },
+  } = useForm<ISignupFormSecond>({
+    defaultValues: {
+      nick_name: "",
+      introduction: "",
+    },
+  });
+  const onFirstValid = (data: ISignupFormFirst) => {
     if (data.password !== data.verifyPassword) {
       setError("verifyPassword", {
         message: "비밀번호가 일치하지 않습니다",
       });
+    } else {
+      setIsFirstStage(false);
     }
   };
+
+  const onSecondValid = (data: ISignupFormSecond) => {};
   return (
     <S.Container>
       <h1>회원가입</h1>
-      <S.Form onSubmit={handleSubmit(onValid)}>
-        <div>
-          <label>아이디</label>
-          <input
-            {...register("user_name", {
-              required: "아이디를 입력해주세요",
-              pattern: {
-                value: /^[a-zA-Z0-9!@#$%^&*()?_~]{8,20}$/,
-                message: "조건에 맞게 다시 입력해주세요",
-              },
-              onChange: () => trigger("user_name"),
-            })}
-            placeholder="아이디"
-            type="text"
-          />
-          <button>중복검사</button>
-          <div>{errors.user_name?.message}</div>
-        </div>
-        <div>
-          <label>비밀번호</label>
-          <input
-            {...register("password", {
-              required: "비밀번호를 입력해주세요",
-              pattern: {
-                value: /^[a-zA-Z0-9!@#$%^&*()?_~]{10,20}$/,
-                message: "조건에 맞게 다시 입력해주세요",
-              },
-              onChange: () => trigger("password"),
-            })}
-            placeholder="비밀번호"
-            type="password"
-          />
-          <div>{errors.password?.message}</div>
-        </div>
-        <div>
-          <label>비밀번호 확인</label>
-          <input
-            {...register("verifyPassword", {
-              required: "비밀번호를 입력해주세요",
-              onChange: () => trigger("verifyPassword"),
-            })}
-            placeholder="비밀번호 확인"
-            type="password"
-          />
-          <div>{errors.verifyPassword?.message}</div>
-        </div>
-        <div>
-          <label>이메일</label>
-          <input
-            {...register("email", {
-              required: "이메일을 입력해주세요",
-              pattern: {
-                value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
-                message: "올바르지 않은 이메일 형식입니다",
-              },
-              onChange: () => trigger("email"),
-            })}
-            placeholder="이메일"
-            type="text"
-          />
-          <div>{errors.email?.message}</div>
-        </div>
-        <div>
-          <label>닉네임</label>
-          <input
-            {...register("nickname", {
-              required: true,
-              minLength: 2,
-              onChange: () => trigger("nickname"),
-            })}
-            placeholder="닉네임"
-            type="text"
-          />
-          <div>{errors.nickname?.message}</div>
-        </div>
-        <div>
-          <label>한줄소개</label>
-          <textarea {...register("introduction")} placeholder="한줄소개" />
-        </div>
-        <button>가입</button>
-      </S.Form>
+      {isFirstStage ? (
+        <S.Form onSubmit={firstHandleSubmit(onFirstValid)}>
+          <div>
+            <label>이메일</label>
+            <input
+              {...firstRegister("email", {
+                required: "이메일을 입력해주세요",
+                pattern: {
+                  value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
+                  message: "정확하지 않은 이메일입니다",
+                },
+                onChange: () => trigger("email"),
+              })}
+              placeholder="이메일"
+              type="text"
+            />
+            <button>중복검사</button>
+            <div>{errors.email?.message}</div>
+          </div>
+          <div>
+            <label>비밀번호</label>
+            <input
+              {...firstRegister("password", {
+                required: "비밀번호를 입력해주세요",
+                pattern: {
+                  value: /^[a-zA-Z0-9!@#$%^&*()?_~]{6,16}$/,
+                  message: "비밀번호는 영문/숫자 조합 6~16자 입니다",
+                },
+                onChange: () => trigger("password"),
+              })}
+              placeholder="비밀번호"
+              type="password"
+            />
+            <div>{errors.password?.message}</div>
+          </div>
+          <div>
+            <label>비밀번호 확인</label>
+            <input
+              {...firstRegister("verifyPassword", {
+                required: "비밀번호를 입력해주세요",
+                onChange: () => trigger("verifyPassword"),
+              })}
+              placeholder="비밀번호 확인"
+              type="password"
+            />
+            <div>{errors.verifyPassword?.message}</div>
+          </div>
+          <button>가입</button>
+        </S.Form>
+      ) : (
+        <S.Form onSubmit={secondHandleSubmit(onSecondValid)}>
+          <div>
+            <label>닉네임</label>
+            <input
+              {...secondRegister("nick_name", {
+                required: true,
+                minLength: 2,
+                maxLength: 10,
+
+                onChange: () => secondTrigger("nick_name"),
+              })}
+              placeholder="닉네임"
+              type="text"
+            />
+            <div>{secondErrors.nick_name?.message}</div>
+          </div>
+          <div>
+            <label>한줄소개</label>
+            <textarea
+              {...secondRegister("introduction")}
+              placeholder="한줄소개"
+            />
+          </div>
+          <button>제출</button>
+        </S.Form>
+      )}
       <button onClick={handleGoBackClick}>뒤로가기</button>
     </S.Container>
   );
--- src/interfaces/forms.ts
+++ src/interfaces/forms.ts
@@ -1,8 +1,9 @@
-export interface ISignupForm {
-  user_name: string;
+export interface ISignupFormFirst {
+  email: string;
   password: string;
   verifyPassword: string;
-  email: string;
-  nickname: string;
+}
+export interface ISignupFormSecond {
+  nick_name: string;
   introduction: string;
 }
