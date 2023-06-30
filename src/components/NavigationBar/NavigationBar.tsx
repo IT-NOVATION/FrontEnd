6a6ea853a90611e4a3e08c45f0d33ced2fdb3cfc
ZUITOPIA <korhwngml123@naver.com>
2023-06-30 19:47:32 +0900

    feat : 네비 검색창 디자인 완료

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -121,11 +121,10 @@ export default function NavigationBar() {
                         <Block.RowBox>
                             {isSearchClick && (
                                 <>
-                                    <Block.ColumnBox width="100%" height="200px" bgColor="white" alignItems="center">
-                                        <Block.RowBox width="900px" height="62px" bgColor="white">
+                                    <Block.ColumnBox width="100%" height="200px" alignItems="center">
+                                        <Block.RowBox width="900px" height="62px">
                                             <Block.RowBox
-                                                width="112px"
-                                                bgColor="white"
+                                                width="152px"
                                                 justifyContent="space-evenly"
                                                 alignItems="center"
                                                 pointer
@@ -135,33 +134,31 @@ export default function NavigationBar() {
                                             </Block.RowBox>
 
                                             <Input.FormInput
-                                                width="744px"
+                                                width="704px"
                                                 height="100%"
                                                 fontSize="30px"
                                                 color="black"
+                                                placeholder="닉네임을 입력해보세요."
                                             />
 
-                                            <Block.RowBox
-                                                width="48px"
-                                                bgColor="white"
-                                                justifyContent="center"
-                                                alignItems="center"
-                                            >
+                                            <Block.RowBox width="48px" justifyContent="center" alignItems="center">
                                                 <S.Icons src="/icons/search.svg" alt="search" />
                                             </Block.RowBox>
                                         </Block.RowBox>
-                                        <Block.Bar width="900px" height="1px" bgColor="black" />
+                                        <Block.Bar width="900px" height="1px" bgColor="black" margin="0 0 33px 0" />
 
-                                        <Block.RowBox
-                                            width="900px"
-                                            height="148px"
-                                            bgColor="gray"
-                                            justifyContent="center"
-                                        >
-                                            <Block.RowBox width="120px" bgColor="white" justifyContent="center">
-                                                고라니
+                                        <Block.RowBox width="900px" height="148px" justifyContent="center">
+                                            <Block.RowBox
+                                                width="120px"
+                                                height="39px"
+                                                bgColor="gray"
+                                                borderRadius="20px"
+                                                justifyContent="center"
+                                                alignItems="center"
+                                            >
+                                                인기 검색어
                                             </Block.RowBox>
-                                            <Block.RowBox width="450px" bgColor="white">
+                                            <Block.RowBox width="450px" padding="11px 23px">
                                                 결과
                                             </Block.RowBox>
                                         </Block.RowBox>
