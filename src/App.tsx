7a821d6c29ce0f48c9bf861bd1d1be04c8510336
stakbucks <dhassidu11@gmail.com>
2023-07-09 22:25:18 +0900

    fix: navigationbar에서 로그인정보 반영되게 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import NavigationBar from "@components/NavigationBar/NavigationBar";
 import { useQuery } from "@tanstack/react-query";
 import { AccountApi } from "@apis/accountApi";
 import { ILoginState } from "@interfaces/loginState";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import { loginStateAtom } from "@recoil/loginStateAtom";
-import { useEffect } from "react";
 
 function App() {
-  const { data } = useQuery<ILoginState>({
+  const setLoginState = useSetRecoilState(loginStateAtom);
+  useQuery<ILoginState>({
     queryKey: ["loginState"],
-    queryFn: AccountApi.loginState,
+    queryFn: async () => {
+      const data = await AccountApi.loginState();
+      setLoginState(data);
+      return data;
+    },
   });
 
   return (
