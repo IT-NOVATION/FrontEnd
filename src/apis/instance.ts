8fc3528b42b00f0a638f1f9e7c942ab274088ee5
stakbucks <dhassidu11@gmail.com>
2023-07-23 04:49:11 +0900

    feat: 백엔드 배포 주소 instance에 추가

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,11 +1,17 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export const BASE_URL = "http://localhost:8080";
+export const REAL_BASE_URL =
+  "http://ec2-43-201-196-249.ap-northeast-2.compute.amazonaws.com:8080/";
 
 const axiosConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
   headers: {
-    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    Authorization: `Bearer ${
+      localStorage.getItem("accessToken")
+        ? localStorage.getItem("accessToken")
+        : ""
+    }`,
   },
 };
 export const baseApi = axios.create(axiosConfig);
