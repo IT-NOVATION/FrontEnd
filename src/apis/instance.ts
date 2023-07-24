8fa81b8a5f0fb1a42f95ae3c744f93e99c6002c0
stakbucks <dhassidu11@gmail.com>
2023-07-24 19:16:27 +0900

    fix: 백엔드 배포 uri 수정

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export const BASE_URL = "http://localhost:8080";
-export const REAL_BASE_URL = "http://api.its-movietime:8080";
+export const REAL_BASE_URL = "http://api.its-movietime.com:8080";
 
 const axiosConfig: AxiosRequestConfig = {
   baseURL: REAL_BASE_URL,
