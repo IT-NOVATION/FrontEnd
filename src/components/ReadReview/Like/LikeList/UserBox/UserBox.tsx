55a42fbbb98cdadd63a22b41409cad2e3c08b110
stakbucks <dhassidu11@gmail.com>
2023-07-26 15:39:46 +0900

    feat: 리뷰읽기 좋아요 목록 팔로우 기능에도 optimistic update 적용

--- src/components/ReadReview/Like/LikeList/UserBox/UserBox.tsx
+++ src/components/ReadReview/Like/LikeList/UserBox/UserBox.tsx
@@ -1,19 +1,56 @@
+import { MovieLogApi } from "@apis/movieLogApi";
 import FollowBtn from "@components/FollowBtn/FollowBtn";
 import ProfileImg from "@components/User/ProfileImg/ProfileImg";
-import useLoginState from "@hooks/useLoginState";
+import useFollow from "@hooks/useFollow";
 import { ILikeListUser } from "@interfaces/user";
 import { Block, Text } from "@styles/UI";
-import { useNavigate } from "react-router-dom";
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate, useParams } from "react-router-dom";
 type Props = {
   user: ILikeListUser;
 };
 
 export default function UserBox({ user }: Props) {
+  const { reviewId } = useParams();
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const handleNicknameClick = () => {
     navigate(`/movieLog/${user.userId}`);
   };
+  const { mutate: mutateFollow } = useMutation({
+    mutationFn: () => MovieLogApi.follow({ targetUserId: user.userId }),
+    onMutate: () => {
+      const prevReviewTime: ILikeListUser[] = queryClient.getQueryData([
+        "likeList",
+        `${reviewId}`,
+      ]) as ILikeListUser[];
+      const updateData = () => {
+        const temp = [...prevReviewTime];
+        let targetUser: ILikeListUser = prevReviewTime?.filter(
+          (v) => v.userId === user.userId
+        )[0];
+        let targetIndex = prevReviewTime.findIndex(
+          (v) => v.userId === user.userId
+        );
+        if (targetUser?.isLoginUserFollowed) {
+          // 이미 팔로우중인 경우
+          targetUser = {
+            ...targetUser,
+            isLoginUserFollowed: false,
+          };
+        } else {
+          targetUser = {
+            ...targetUser,
+            isLoginUserFollowed: true,
+          };
+        }
+        temp[targetIndex] = targetUser;
+        return temp;
+      };
+      queryClient.setQueryData(["likeList", `${reviewId}`], updateData());
+    },
+  });
+  const handleFollowClick = useFollow(mutateFollow);
   return (
     <Block.RowBox
       margin="0 0 15px 0"
@@ -27,10 +64,12 @@ export default function UserBox({ user }: Props) {
         </Text.Body4>
       </Block.RowBox>
       {!user.isMyProfile && (
-        <FollowBtn
-          isFollowing={user.isLoginUserFollowed}
-          userId={String(user.userId)}
-        />
+        <Block.RowBox onClick={handleFollowClick} width="auto">
+          <FollowBtn
+            isFollowing={user.isLoginUserFollowed}
+            userId={String(user.userId)}
+          />
+        </Block.RowBox>
       )}
     </Block.RowBox>
   );
